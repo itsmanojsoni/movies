92bba4db271a7e6643269ad85fcfe99aead7052b
manoj <manoj@flipagram.com>
2017-09-15 19:28:50 -0700

    finally got the movie list

--- app/src/main/java/com/codepath/movies/MainActivity.java
+++ app/src/main/java/com/codepath/movies/MainActivity.java
@@ -2,12 +2,53 @@ package com.codepath.movies;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private static  final String TAG = "MainActivity";
+    // TODO - insert your themoviedb.org API KEY here
+    private final static String API_KEY = "7e8f60e325cd06e164799af1e317d7a7";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        final RecyclerView recyclerView = (RecyclerView) findViewById(R.id.movies_recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        ApiInterface apiService =
+                ApiClient.getClient().create(ApiInterface.class);
+
+        Call<MoviesResponse> call = apiService.getTopRatedMovies(API_KEY);
+        call.enqueue(new Callback<MoviesResponse>() {
+            @Override
+            public void onResponse(Call<MoviesResponse> call, Response<MoviesResponse> response) {
+                int statusCode = response.code();
+                List<Movie> movies = response.body().getResults();
+
+                Log.d(TAG, "Movie List Size = "+movies.size());
+
+//                recyclerView.setAdapter(new MoviesAdapter(movies, R.layout.list_item_movie, getApplicationContext()));
+            }
+
+            @Override
+            public void onFailure(Call<MoviesResponse> call, Throwable t) {
+                // Log error here since request failed
+                Log.e(TAG, t.toString());
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/com/codepath/movies/MoviesResponse.java
@@ -0,0 +1,19 @@
+package com.codepath.movies;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+/**
+ * Created by manoj on 9/15/17.
+ */
+
+public class MoviesResponse {
+
+    public List<Movie> getResults() {
+        return results;
+    }
+
+    @SerializedName("results")
+    List<Movie> results;
+}
